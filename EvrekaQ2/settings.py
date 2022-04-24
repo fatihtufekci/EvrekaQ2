79a85504f719bb5df2061abfd4fca84e1f3edf6e
Fatih Tüfekçi <fatihtufekci1997@gmail.com>
2022-04-24 23:00:33 +0300

    created bin_operation app

--- EvrekaQ2/settings.py
+++ EvrekaQ2/settings.py
@@ -29,6 +29,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'django_extensions',
+
+    'bin_operation.apps.BinOperationConfig'
 ]
 
 MIDDLEWARE = [
---
+++ bin_operation/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BinOperationConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'bin_operation'

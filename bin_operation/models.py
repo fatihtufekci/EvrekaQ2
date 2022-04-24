e84d69932b88836d8b6cebb37cefd839759c03b6
Fatih Tüfekçi <fatihtufekci1997@gmail.com>
2022-04-25 01:56:15 +0300

    bin_operation models created

--- bin_operation/models.py
+++ bin_operation/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Bin(models.Model):
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+    def __str__(self):
+        return f'{self.latitude} - {self.longitude}'
+
+class Operation(models.Model):
+    name = models.CharField(max_length=120)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class BinOperation(models.Model):
+    operation = models.ForeignKey(Operation, on_delete=models.CASCADE, related_name="operation_bins")
+    bin = models.ForeignKey(Bin, on_delete=models.CASCADE, related_name="bin_operations")
+    collection_frequency = models.IntegerField(default=0)
+    last_collection = models.DateTimeField()
+
+    def __str__(self):
+        return f'{self.operation.name} - ({self.bin.longitude},{self.bin.latitude}) - {self.collection_frequency} - {self.last_collection.strftime("%Y-%m-%d %H:%M:%S")}'
